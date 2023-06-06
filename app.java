49c8af2a2e88abe15b05c5d2e2f59300a35747d1
Bensen Young <bensenmyoung@gmail.com>
2023-06-05 23:28:23 -0600

    modified

--- app.java
+++ app.java
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
-This app is excellent for all users
-=======
+
+This app is excellent for all users.
 We support;
 java applications,
 .net applications,
 NodeJS applications,
-python applications ,
-web applicaiton
-bug removed
->>>>>>> master
-great job
+python applications,
+web applicaitons,
+This app is excellent for all users.
+great job.
